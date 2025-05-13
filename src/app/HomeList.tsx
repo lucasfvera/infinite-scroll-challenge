55d6472289b61bed52bd7bdfb999905997de8ece
lucasfv <lucasferreyravera@gmail.com>
2025-05-13 17:26:52 -0300

    fix: Change observer position
    We had the observer to trigger fetching the next page in the last
    element of the list. This causes the users to get to the bottom and
    wait for the fetching to finish.
    
    This commit moves the observer to the middle of the list instead to
    ensure users have a smoother experience when scrolling since the
    fetch will be triggered when they reach the middle of the page.

--- src/app/HomeList.tsx
+++ src/app/HomeList.tsx
@@ -38,42 +38,45 @@ export default function HomeList() {
 		}
 	}, [inView, fetchNextPage, hasNextPage]);
 
+	// TODO - If we want a cleaner code and make it more readable, we could extract
+	// the list item into it's own component
 	return (
-		<>
-			<ul
-				style={{
-					border: '1px solid black',
-					padding: '8px',
-					margin: '0 16px',
-				}}
-			>
-				{data &&
-					data.pages.map((page) =>
-						page.data.map(
-							({ id, address, homeowner, price }, index) => (
-								<li
-									key={id}
-									// We set the observer to the last element
-									// in the list
-									ref={
-										page.data.length === index + 1
-											? ref
-											: null
-									}
-									style={{
-										border: '1px solid blue',
-										marginBottom: '8px',
-										padding: '4px',
-									}}
-								>
-									<p>{homeowner}</p>
-									<p>{address}</p>
-									<p>${price}</p>
-								</li>
-							)
+		<ul
+			style={{
+				border: '1px solid black',
+				padding: '8px',
+				margin: '0 16px',
+			}}
+		>
+			{data &&
+				data.pages.map((page) =>
+					page.data.map(
+						({ id, address, homeowner, price }, index) => (
+							<li
+								key={id}
+								// We set the observer to the element in the
+								// middle of the list to ensure we have
+								// the next page before the user gets to the bottom
+								ref={
+									Math.floor(page.data.length / 2) ===
+									index + 1
+										? ref
+										: null
+								}
+								style={{
+									border: '1px solid blue',
+									marginBottom: '8px',
+									padding: '4px',
+								}}
+							>
+								<p>{id}</p>
+								<p>{homeowner}</p>
+								<p>{address}</p>
+								<p>${price}</p>
+							</li>
 						)
-					)}
-			</ul>
-		</>
+					)
+				)}
+		</ul>
 	);
 }
